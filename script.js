7355765d9005317c2b174cd2e8c2c902cca704a0
Madhu <madhumahato1999s@gmail.com>
2023-11-26 13:27:41 +0530

    line addede

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ window.addEventListener("load", () => fetchNews("India"));
 function reload() {
   window.location.reload();
 }
+// new line added to fix cors//
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/news", async (req, res) => {
+  const { query } = req.query;
+  const apiKey = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+  const apiUrl = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 async function fetchNews(query) {
   const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
